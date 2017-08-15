b14a0b4e317b4a1da49293e5bee098ce5256772b
Tanuj <vishnoitanuj@outlook.com>
2017-08-16 00:44:39 +0530

    reload added with errors

---
+++ js/view.js
@@ -0,0 +1,12 @@
+/**
+ * Created by TANUJ on 15-08-2017.
+ */
+function farmer(){
+    document.getElementById("r1").style.display="block";
+    document.getElementById("r2").style.display="none";
+}
+
+function other() {
+    document.getElementById("r1").style.display="none";
+    document.getElementById("r2").style.display="block";
+}
